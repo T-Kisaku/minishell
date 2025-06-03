569688272e6e6724595b09d2b141bdb1e71694c8
Takashi Kisaku <kisakutakashi0226@gmail.com>
2025-06-03 11:37:55 +0900

    update ast.h

--- include/ast.h
+++ include/ast.h
@@ -40,6 +40,7 @@
 #define AST_H
 
 #include <stddef.h>
+#include "token.h"
 
 typedef enum e_redir_type {
   REDIR_INPUT,
@@ -53,10 +54,7 @@ typedef struct s_redir {
   char *target;
 } t_redir;
 
-typedef enum e_cmd_type {
-  CMD_SIMPLE,
-  CMD_SUBSHELL
-} t_cmd_type;
+typedef enum e_cmd_type { CMD_SIMPLE, CMD_SUBSHELL } t_cmd_type;
 
 typedef struct s_list t_list;
 
@@ -67,27 +65,23 @@ typedef struct s_command {
 
   union {
     struct {
-      char **argv; 
+      t_token_list *token_list; // When you pass this to Executor, free this.
+      char **argv;              // Malloc from token_list
       int argc;
     } simple;
 
     struct {
-      t_list *list; 
+      t_list *list;
     } subshell;
   } u;
 } t_command;
 
 typedef struct s_pipeline {
-  t_command *
-      commands;
+  t_command *commands;
   int cmd_count;
 } t_pipeline;
 
-typedef enum e_andor_op {
-  OP_NONE,
-  OP_AND,
-  OP_OR 
-} t_andor_op;
+typedef enum e_andor_op { OP_NONE, OP_AND, OP_OR } t_andor_op;
 
 typedef struct s_and_or {
   t_pipeline pipeline;
@@ -96,8 +90,8 @@ typedef struct s_and_or {
 } t_and_or;
 
 typedef struct s_list {
-  t_and_or *first_and_or; 
-  struct s_list *next; 
+  t_and_or *first_and_or;
+  struct s_list *next;
 } t_list;
 
 typedef t_list t_ast;
