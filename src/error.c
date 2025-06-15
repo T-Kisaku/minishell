e386c8c44797caed8c38685fd619606ca4af7f24
Takashi Kisaku <kisakutakashi0226@gmail.com>
2025-06-15 12:10:25 +0900

    feature: error and exit status

--- src/error.c
+++ src/error.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
-#include "error.h"
 #include "libft.h"
+#include "error.h"
 
-int internal_err(char *s) {
-  perror(s);
-  return EXIT_INTERNAL_ERR;
-}
-
-int user_err(char *msg) {
-  if (write(STDERR_FILENO, msg, ft_strlen(msg)) < 0)
-    return internal_err(ERR_MSG_WRITE);
-  return EXIT_USER_ERR;
+// Return 0 if usr_error succed
+int usr_error(char *msg) {
+  if (write(STDERR_FILENO, msg, ft_strlen(msg)) < 0) {
+    perror(ERR_MSG_WRITE);
+    return -1;
+  }
+  return (0);
 }
--- src/error.h
+++ src/error.h
@@ -1,15 +1,11 @@
-#ifndef MS_ERR_H
-#define MS_ERR_H
 
-#define EXIT_OK 0            // Success
-#define EXIT_INTERNAL_ERR 1  // Internal error (e.g., malloc failure)
-#define EXIT_USER_ERR 2      // User error (e.g., input or syntax error)
-#define EXIT_EXEC_FAIL 126   // Execution failure (e.g., permission denied)
-#define EXIT_NOT_FOUND 127   // Command not found
-#define EXIT_SIGNAL_BASE 128 // Exit caused by signal (add signal number)
+#ifndef MS_ERROR_H
+#define MS_ERROR_H
 
 #define ERR_MSG_MINISHEL "minishell: "
 #define ERR_MSG_WRITE ERR_MSG_MINISHEL "write"
 #define ERR_MSG_MALLOC ERR_MSG_MINISHEL "malloc"
 
-#endif // !MS_ERR_H
+int usr_error(char *msg);
+
+#endif // !MS_ERROR_H
