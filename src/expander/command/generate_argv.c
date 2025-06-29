e19a33c060d83b7fd42a9026d98acbeeb6c12574
Sayu Ueda <saueda@student.42tokyo.jp>
2025-06-29 14:49:43 +0900

    pass norm

--- src/expander/command/generate_argv.c
+++ src/expander/command/generate_argv.c
@@ -6,7 +6,7 @@
 /*   By: saueda <saueda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 08:52:59 by tkisaku           #+#    #+#             */
-/*   Updated: 2025/06/29 14:35:13 by saueda           ###   ########.fr       */
+/*   Updated: 2025/06/29 14:44:01 by saueda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ t_error	*generate_argv_handler(t_command *cmd, t_minishell_state *shell)
 			return (error);
 	}
 	else if (cmd->type == CMD_SUBSHELL)
-    return NULL;
+		return (NULL);
 	if (cmd->u.simple.argv == NULL)
 		return (new_error(EXIT_INTERNAL_ERR, "MALLOC ERRO"));
 	else
