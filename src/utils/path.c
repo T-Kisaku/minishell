54dac39bcd46567ba31725ef9d82a9ee6a4d8398
Takashi Kisaku <kisakutakashi0226@gmail.com>
2025-06-30 13:26:31 +0900

    add validate infile

--- src/utils/path.c
+++ src/utils/path.c
@@ -6,7 +6,7 @@
 /*   By: saueda <saueda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/29 08:52:57 by tkisaku           #+#    #+#             */
-/*   Updated: 2025/06/30 10:26:07 by tkisaku          ###   ########.fr       */
+/*   Updated: 2025/06/30 13:26:08 by tkisaku          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,4 +43,20 @@ int	is_cd_target_valid(const char *path)
 		return (0);
 	}
 	return (1);
+}
+
+int	validate_infile(const char *infile)
+{
+	if (access(infile, F_OK) != 0)
+	{
+		ft_dprintf(STDERR_FILENO, "Error: file '%s' does not exist.\n", infile);
+		return (-1);
+	}
+	if (access(infile, R_OK) != 0)
+	{
+		ft_dprintf(STDERR_FILENO, "Error: file '%s' is not readable.\n",
+			infile);
+		return (-1);
+	}
+	return (0);
 }
