b2ae31823c7111b33337ff368023f7fe5798daef
Takashi Kisaku <kisakutakashi0226@gmail.com>
2025-07-01 15:09:15 +0900

    update push shell

--- mk/push-to-vogsphere.sh
+++ mk/push-to-vogsphere.sh
@@ -3,7 +3,6 @@ REPO_URL="git@vogsphere-v2.42tokyo.jp:vogsphere/intra-uuid-384626b4-8483-4b76-99
 HASH_FILE="git-hash.txt"
 git rev-parse --short HEAD >$HASH_FILE
 WORK_DIR="../minishell-$(cat $HASH_FILE)"
-# TMP_DIR="../minishell-tmp-$(cat $HASH_FILE)"
 
 git clone "$REPO_URL" "$WORK_DIR"
 rsync -a --exclude='.git' . "$WORK_DIR"
